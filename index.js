97a6e3be35a7893eb9fcaab81f6a0a9921ea9859
Xavier Ritch <xritch89@hotmail.com>
2020-08-10 18:07:59 -0400

    added labels and removed map CDN

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-// Initialize and add the map, but no API key so its not going to work :(
-function initMap() {
-    // The location of Uluru
-    let uluru = {lat: -25.344, lng: 131.036};
-    // The map, centered at Uluru
-    let map = new google.maps.Map(
-        document.getElementById('map'), {zoom: 4, center: uluru});
-    // The marker, positioned at Uluru
-    let marker = new google.maps.Marker({position: uluru, map: map});
-  }
+// // Initialize and add the map, but no API key so its not going to work :(
+// function initMap() {
+//     // The location of Uluru
+//     let uluru = {lat: -25.344, lng: 131.036};
+//     // The map, centered at Uluru
+//     let map = new google.maps.Map(
+//         document.getElementById('map'), {zoom: 4, center: uluru});
+//     // The marker, positioned at Uluru
+//     let marker = new google.maps.Marker({position: uluru, map: map});
+//   }
